6fc8add08d370c034d220b3518cee2d2d7849fd3
Enayet <enayetflweb@gamil.com>
2023-12-21 08:19:52 +0600

    footer complete

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,8 +1,50 @@
+import * as React from 'react';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import WorkIcon from '@mui/icons-material/Work';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import { Link } from 'react-router-dom';
+
 const Footer = () => {
+  const [value, setValue] = React.useState('portfolio');
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
   return (
-    <div>
-Footer      
-    </div>
+    <BottomNavigation 
+    sx={{
+      borderTop:'2px solid #444',
+      borderBottom:'2px solid #444'
+    }}
+    value={value} onChange={handleChange}>
+
+          <BottomNavigationAction
+        label="Portfolio"
+        value="portfolio"
+        icon={<Link to={'https://enayet-portfolio.netlify.app/'}><WorkIcon /></Link>}
+        />
+     
+     <BottomNavigationAction
+        label="Linkedin"
+        value="linkedin"
+        icon={<Link to={'https://www.linkedin.com/in/md-enayetur-rahman/'}><LinkedInIcon /></Link>}
+      />
+
+      <BottomNavigationAction
+        label="Facebook"
+        value="facebook"
+        icon={<Link to={'https://www.facebook.com/profile.php?id=100094416483981'}><FacebookIcon /></Link>}
+      />
+
+     
+     <BottomNavigationAction label="Twitter" value="twitter" icon={<Link to={'https://twitter.com/enayetu_syl'}><TwitterIcon /></Link>}
+      />
+   
+    </BottomNavigation>
   );
 };
 
